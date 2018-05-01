3890322192c514f45d70a0f962dcd861a4cd7c31
rushTENm <rushtenm@gmail.com>
2018-05-02 01:01:45 +0900

    Reformat Entire Code

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -15,9 +15,11 @@ public class Alarm {
      * alarm.
      */
     public Alarm() {
-	Machine.timer().setInterruptHandler(new Runnable() {
-		public void run() { timerInterrupt(); }
-	    });
+        Machine.timer().setInterruptHandler(new Runnable() {
+            public void run() {
+                timerInterrupt();
+            }
+        });
     }
 
     /**
@@ -27,7 +29,7 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+        KThread.currentThread().yield();
     }
 
     /**
@@ -40,14 +42,13 @@ public class Alarm {
      * (current time) >= (WaitUntil called time)+(x)
      * </blockquote>
      *
-     * @param	x	the minimum number of clock ticks to wait.
-     *
-     * @see	nachos.machine.Timer#getTime()
+     * @param    x    the minimum number of clock ticks to wait.
+     * @see    nachos.machine.Timer#getTime()
      */
     public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+        // for now, cheat just to get something working (busy waiting is bad)
+        long wakeTime = Machine.timer().getTime() + x;
+        while (wakeTime > Machine.timer().getTime())
+            KThread.yield();
     }
 }
--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -19,12 +19,12 @@ public class Communicator {
     /**
      * Wait for a thread to listen through this communicator, and then transfer
      * <i>word</i> to the listener.
-     *
+     * <p>
      * <p>
      * Does not return until this thread is paired up with a listening thread.
      * Exactly one listener should receive <i>word</i>.
      *
-     * @param	word	the integer to transfer.
+     * @param    word    the integer to transfer.
      */
     public void speak(int word) {
     }
@@ -33,9 +33,9 @@ public class Communicator {
      * Wait for a thread to speak through this communicator, and then return
      * the <i>word</i> that thread passed to <tt>speak()</tt>.
      *
-     * @return	the integer transferred.
-     */    
+     * @return the integer transferred.
+     */
     public int listen() {
-	return 0;
+        return 0;
     }
 }
