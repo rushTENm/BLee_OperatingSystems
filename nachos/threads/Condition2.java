4b85077b16e99b2dc187e55b2cef36b9120c6877
SeoYH <wjdghks1057@naver.com>
2018-05-29 00:07:10 +0900

    project #2-2

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -1,58 +1,81 @@
 package nachos.threads;
 
+import java.util.LinkedList;
 import nachos.machine.*;
 
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
- * <p>
+ *
  * <p>
  * You must implement this.
  *
- * @see    nachos.threads.Condition
+ * @see nachos.threads.Condition
  */
 public class Condition2 {
-    /**
-     * Allocate a new condition variable.
-     *
-     * @param    conditionLock    the lock associated with this condition
-     * variable. The current thread must hold this
-     * lock whenever it uses <tt>sleep()</tt>,
-     * <tt>wake()</tt>, or <tt>wakeAll()</tt>.
-     */
-    public Condition2(Lock conditionLock) {
-        this.conditionLock = conditionLock;
-    }
+	/**
+	 * Allocate a new condition variable.
+	 *
+	 * @param conditionLock
+	 *            the lock associated with this condition variable. The current
+	 *            thread must hold this lock whenever it uses <tt>sleep()</tt>,
+	 *            <tt>wake()</tt>, or <tt>wakeAll()</tt>.
+	 */
+	public Condition2(Lock conditionLock) {
+		this.conditionLock = conditionLock;
+	}
 
-    /**
-     * Atomically release the associated lock and go to sleep on this condition
-     * variable until another thread wakes it using <tt>wake()</tt>. The
-     * current thread must hold the associated lock. The thread will
-     * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     */
-    public void sleep() {
-        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	/**
+	 * Atomically release the associated lock and go to sleep on this condition
+	 * variable until another thread wakes it using <tt>wake()</tt>. The current
+	 * thread must hold the associated lock. The thread will automatically
+	 * reacquire the lock before <tt>sleep()</tt> returns.
+	 */
+	public void sleep() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-        conditionLock.release();
+		conditionLock.release();
+		boolean intStatus = Machine.interrupt().disable();
+		CV_WaitThread_List.add(KThread.currentThread());
+		KThread.sleep();
+		Machine.interrupt().restore(intStatus);
+		conditionLock.acquire();
 
-        conditionLock.acquire();
-    }
+	}
 
-    /**
-     * Wake up at most one thread sleeping on this condition variable. The
-     * current thread must hold the associated lock.
-     */
-    public void wake() {
-        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+	/**
+	 * Wake up at most one thread sleeping on this condition variable. The
+	 * current thread must hold the associated lock.
+	 */
+	public void wake() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-    /**
-     * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
-     */
-    public void wakeAll() {
-        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+		conditionLock.release();
+		boolean intStatus = Machine.interrupt().disable();
+		if (!CV_WaitThread_List.isEmpty())
+			CV_WaitThread_List.removeFirst().ready();
+		Machine.interrupt().restore(intStatus);
+		conditionLock.acquire();
 
-    private Lock conditionLock;
+	}
+
+	/**
+	 * Wake up all threads sleeping on this condition variable. The current
+	 * thread must hold the associated lock.
+	 */
+	public void wakeAll() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+
+		conditionLock.release();
+		boolean intStatus = Machine.interrupt().disabled();
+		while (!CV_WaitThread_List.isEmpty())
+			wake();
+		Machine.interrupt().restore(intStatus);
+		conditionLock.acquire();
+
+	}
+
+	private Lock conditionLock;
+	private LinkedList<KThread> CV_WaitThread_List = new LinkedList<>();
 }
--- nachos/threads/ThreadedKernel.java
+++ nachos/threads/ThreadedKernel.java
@@ -6,112 +6,136 @@ import nachos.machine.*;
  * A multi-threaded OS kernel.
  */
 public class ThreadedKernel extends Kernel {
-    /**
-     * Allocate a new multi-threaded kernel.
-     */
-    public ThreadedKernel() {
-        super();
-    }
+	/**
+	 * Allocate a new multi-threaded kernel.
+	 */
+	public ThreadedKernel() {
+		super();
+	}
 
-    /**
-     * Initialize this kernel. Creates a scheduler, the first thread, and an
-     * alarm, and enables interrupts. Creates a file system if necessary.
-     */
-    public void initialize(String[] args) {
-        // set scheduler
-        String schedulerName = Config.getString("ThreadedKernel.scheduler");
-        scheduler = (Scheduler) Lib.constructObject(schedulerName);
+	/**
+	 * Initialize this kernel. Creates a scheduler, the first thread, and an
+	 * alarm, and enables interrupts. Creates a file system if necessary.
+	 */
+	public void initialize(String[] args) {
+		// set scheduler
+		String schedulerName = Config.getString("ThreadedKernel.scheduler");
+		scheduler = (Scheduler) Lib.constructObject(schedulerName);
 
-        // set fileSystem
-        String fileSystemName = Config.getString("ThreadedKernel.fileSystem");
-        if (fileSystemName != null)
-            fileSystem = (FileSystem) Lib.constructObject(fileSystemName);
-        else if (Machine.stubFileSystem() != null)
-            fileSystem = Machine.stubFileSystem();
-        else
-            fileSystem = null;
+		// set fileSystem
+		String fileSystemName = Config.getString("ThreadedKernel.fileSystem");
+		if (fileSystemName != null)
+			fileSystem = (FileSystem) Lib.constructObject(fileSystemName);
+		else if (Machine.stubFileSystem() != null)
+			fileSystem = Machine.stubFileSystem();
+		else
+			fileSystem = null;
 
-        // start threading
-        new KThread(null);
+		// start threading
+		new KThread(null);
 
-        alarm = new Alarm();
+		alarm = new Alarm();
 
-        Machine.interrupt().enable();
-    }
+		Machine.interrupt().enable();
+	}
 
-    /**
-     * Test this kernel. Test the <tt>KThread</tt>, <tt>Semaphore</tt>,
-     * <tt>SynchList</tt>, and <tt>ElevatorBank</tt> classes. Note that the
-     * autograder never calls this method, so it is safe to put additional tests
-     * here.
-     */
-    /* PingTest 클래스는 새롭게 추가한 내용임 */
-    private static class PingTest implements Runnable {
-        PingTest(int which) {
-            this.which = which;
-        }
+	/**
+	 * Test this kernel. Test the <tt>KThread</tt>, <tt>Semaphore</tt>,
+	 * <tt>SynchList</tt>, and <tt>ElevatorBank</tt> classes. Note that the
+	 * autograder never calls this method, so it is safe to put additional tests
+	 * here.
+	 */
+	/* PingTest 클래스는 새롭게 추가한 내용임 */
 
-        public void run() {
-            for (int i = 0; i < 3; i++) {
-                System.out.println("*** thread " + which + " looped " + i + " times");
-                KThread.currentThread().yield();
-            }
-        }
+	private static class PingTest implements Runnable {
+		PingTest(int which) {
+			this.which = which;
+		}
 
-        private int which;
-    }
+		public void run() {
+			for (int i = 0; i < 3; i++) {
+				System.out.println("*** thread " + which + " looped " + i + " times");
+				KThread.currentThread().yield();
+			}
+		}
 
-    public void selfTest() {
-        KThread thread2 = new KThread(new PingTest(2)).setName("forked 1");
-        KThread thread3 = new KThread(new PingTest(3)).setName("forked 2");
-        KThread thread4 = new KThread(new PingTest(4)).setName("forked 3");
+		private int which;
+	}
 
-        boolean status = Machine.interrupt().disable();
-        scheduler.setPriority(thread2, 2);
-        scheduler.setPriority(thread3, 3);
-        scheduler.setPriority(thread4, 2);
-        thread4.fork();
-        thread3.fork();
-        thread2.fork();
-        Machine.interrupt().restore(status);
+	private static class Communicator_test implements Runnable {
+		Communicator_test(Condition2 condi, Condition2 condi2, Lock lock, int id) {
+			human = new Communicator();
+			which = id;
+		}
 
-        new PingTest(1).run();
-    }
+		public void run() {
+			for (int i = 0; i < 3; i++) {
+				// 짝수 speak, 홀수 listen
+				if (which % 2 == 0)
+					human.speak(which);
+				else
+					human.listen();
+			}
+		}
 
+		private int which;
+		private Communicator human = null;
+	}
 
-    /**
-     * A threaded kernel does not run user programs, so this method does
-     * nothing.
-     */
-    public void run() {
-    }
+	/* 기존의 selfTest() 부분은 모두 주석으로 처리하고 PingTest Thread 부분 새롭게 추가 */
+	public void selfTest() {
 
-    /**
-     * Terminate this kernel. Never returns.
-     */
-    public void terminate() {
-        Machine.halt();
-    }
+		mutex = new Lock();
+		speakCon = new Condition2(mutex);
+		listenCon = new Condition2(mutex);
 
-    /**
-     * Globally accessible reference to the scheduler.
-     */
-    public static Scheduler scheduler = null;
-    /**
-     * Globally accessible reference to the alarm.
-     */
-    public static Alarm alarm = null;
-    /**
-     * Globally accessible reference to the file system.
-     */
-    public static FileSystem fileSystem = null;
+		KThread communicator1, communicator2, communicator3, communicator4;
+		communicator1 = new KThread(new Communicator_test(speakCon, listenCon, mutex, 0));
+		communicator2 = new KThread(new Communicator_test(speakCon, listenCon, mutex, 1));
+		communicator3 = new KThread(new Communicator_test(speakCon, listenCon, mutex, 2));
+		communicator4 = new KThread(new Communicator_test(speakCon, listenCon, mutex, 3));
+		communicator1.fork();
+		communicator2.fork();
+		communicator3.fork();
+		communicator4.fork();
+		communicator1.join();
+		communicator2.join();
+		communicator3.join();
+		communicator4.join();
+	}
 
-    // dummy variables to make javac smarter
-    private static RoundRobinScheduler dummy1 = null;
-    private static PriorityScheduler dummy2 = null;
-    private static LotteryScheduler dummy3 = null;
-    private static Condition2 dummy4 = null;
-    private static Communicator dummy5 = null;
-    private static Rider dummy6 = null;
-    private static ElevatorController dummy7 = null;
+	public static Condition2 speakCon = null;
+	public static Condition2 listenCon = null;
+	public static Lock mutex = null;
+	public static int communicator_id = 0;
+
+	/**
+	 * A threaded kernel does not run user programs, so this method does
+	 * nothing.
+	 */
+	public void run() {
+	}
+
+	/**
+	 * Terminate this kernel. Never returns.
+	 */
+	public void terminate() {
+		Machine.halt();
+	}
+
+	/** Globally accessible reference to the scheduler. */
+	public static Scheduler scheduler = null;
+	/** Globally accessible reference to the alarm. */
+	public static Alarm alarm = null;
+	/** Globally accessible reference to the file system. */
+	public static FileSystem fileSystem = null;
+
+	// dummy variables to make javac smarter
+	private static RoundRobinScheduler dummy1 = null;
+	private static PriorityScheduler dummy2 = null;
+	private static LotteryScheduler dummy3 = null;
+	private static Condition2 dummy4 = null;
+	private static Communicator dummy5 = null;
+	private static Rider dummy6 = null;
+	private static ElevatorController dummy7 = null;
 }
